23862e196d4d5475b796cbd464cf195c70a8c1c7
panscan <joeavanzato@gmail.com>
2023-10-25 13:57:24 -0400

    - General improvements, starting to look at 'abnormal' locations - needs major refinement.

--- internal/findSuspiciousConnections.go
+++ internal/findSuspiciousConnections.go
@@ -2,20 +2,23 @@ package internal
 
 import (
 	"github.com/bastjan/netstat"
+	"github.com/javanzato/crackdown/internal/helpers"
 	"github.com/sirupsen/logrus"
+	"strconv"
+	"strings"
 )
 
-var suspiciousPorts = map[int]string{
-	20:   "UDP",
-	21:   "FTP",
-	22:   "SSH",
-	23:   "TELNET",
-	25:   "SMTP",
-	53:   "DNS",
-	137:  "NETBIOS",
-	139:  "SMB",
-	445:  "SMB",
-	3389: "RDP",
+var suspiciousPorts = map[int]bool{
+	20:   true,
+	21:   true,
+	22:   true,
+	23:   true,
+	25:   true,
+	53:   true,
+	137:  true,
+	139:  true,
+	445:  true,
+	3389: true,
 }
 
 func FindSuspiciousConnections(logger *logrus.Logger, detections chan<- Detection, waitGroup *WaitGroupCount) {
@@ -28,9 +31,66 @@ func FindSuspiciousConnections(logger *logrus.Logger, detections chan<- Detectio
 		logger.Error(err)
 		return
 	}
+	skipIPs := map[string]bool{
+		"0.0.0.0":   true,
+		"127.0.0.1": true,
+	}
+	suspiciousExeLocations := []string{
+		"/home/games",
+		"/home/lib",
+		"/home/local",
+		"/home/sbin",
+		"/home/share",
+		"/home/src",
+		"/usr/",
+	}
+	appAllowList := []string{
+		"usr/lib/firefox/firefox",
+	}
+
 	for _, v := range connections {
-		if string(v.RemoteIP) == "0.0.0.0" {
+		if skipIPs[v.RemoteIP.String()] {
 			continue
+		}
+		tmp_ := map[string]string{
+			"RemoteAddress": v.RemoteIP.String(),
+			"LocalPort":     strconv.Itoa(v.Port),
+			"RemotePort":    strconv.Itoa(v.RemotePort),
+			"Executable":    v.Exe,
+			"Commandline":   strings.Join(v.Cmdline, " "),
+		}
+		detection := Detection{
+			Name:      "Executable in Home Directory with Network Connection",
+			Severity:  2,
+			Tip:       "Investigate connection to determine validity.",
+			Technique: "T1071",
+			Metadata:  tmp_,
+		}
+		allowMatch := false
+	allowPattern:
+		for _, loc := range appAllowList {
+			if helpers.SearchStringContains(v.Exe, loc) {
+				allowMatch = true
+				break allowPattern
+			}
+		}
+		if allowMatch {
+			continue
+		}
+
+		if suspiciousPorts[v.Port] {
+			detection.Name = "Connection on Suspicious Port"
+			detections <- detection
+			continue
+		}
+	patternMatch:
+		for _, loc := range suspiciousExeLocations {
+			if helpers.SearchStringContains(v.Exe, loc) {
+				detection.Name = "Executable in abnormal directory with network connection"
+				detection.Metadata["Pattern"] = loc
+				detections <- detection
+				break patternMatch
+			}
 		}
 		//logger.Info(v)
 	}
