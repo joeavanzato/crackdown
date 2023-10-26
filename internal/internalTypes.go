eaf06685206011cf91381b2984a0de12b86a8f54
panscan <joeavanzato@gmail.com>
2023-10-25 22:54:45 -0400

    - Abstracting suspicious patterns for general usage

--- internal/internalTypes.go
+++ internal/internalTypes.go
@@ -26,6 +26,34 @@ func (d Detection) MarshalZerologObject(e *zerolog.Event) {
 
 }
 
+var suspiciousPatterns = []string{
+	"BEGIN {s = \"/inet/tcp/0/",
+	"bash -i >& /dev/tcp/",
+	"bash -i >& /dev/udp/",
+	"sh -i >$ /dev/udp/",
+	"sh -i >$ /dev/tcp/",
+	"&& while read line 0<&5; do",
+	"/bin/bash -c exec 5<>/dev/tcp/",
+	"/bin/bash -c exec 5<>/dev/udp/",
+	"nc -e /bin/sh ",
+	"/bin/sh | nc",
+	"rm -f backpipe; mknod /tmp/backpipe p && nc ",
+	";socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i))))",
+	";STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;",
+	"/bin/sh -i <&3 >&3 2>&3",
+	"uname -a; w; id; /bin/bash -i",
+	"([text.encoding]::ASCII).GetBytes",
+	"$stream.Flush()",
+	"exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)",
+	"while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print",
+	"socat exec:''bash -li'',pty,stderr,setsid,sigint,sane tcp",
+	"rm -f /tmp/p; mknod /tmp/p p &&",
+	"/bin/bash | telnet",
+	"echo=0,raw tcp-listen:",
+	"nc -lvvp",
+	"xterm -display 1",
+}
+
 func (d Detection) String() string {
 	// Format the string of a detection to properly iterate over the Metadata when rendering
 	// TODO - Ths second loop after sort for some reason is introducing empty keys to the array - not sure why.
