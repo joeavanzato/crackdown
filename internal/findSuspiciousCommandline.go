4b7b35115007d7da3a5500dd9300584207df035a
panscan <joeavanzato@gmail.com>
2023-10-27 11:56:46 -0400

    - Getting full exe path to file - Adding detection for running process that does not have an existing file on disk - Adding recently modified binary for running process detection.

--- internal/findSuspiciousCommandline.go
+++ internal/findSuspiciousCommandline.go
@@ -1,12 +1,15 @@
 package internal
 
 import (
+	"errors"
 	"fmt"
 	"github.com/javanzato/crackdown/internal/helpers"
 	"github.com/mitchellh/go-ps"
 	"github.com/rs/zerolog"
-	"regexp"
+	"os"
+	"os/exec"
 	"strconv"
+	"strings"
 )
 
 func FindSuspiciousCommandlines(logger zerolog.Logger, detections chan<- Detection, waitGroup *WaitGroupCount) {
@@ -28,9 +31,24 @@ func FindSuspiciousCommandlines(logger zerolog.Logger, detections chan<- Detecti
 		cmdlineLocation := fmt.Sprintf("/proc/%d/cmdline", process.Pid())
 		cmdline := helpers.ReadFileToString(cmdlineLocation, logger)
 		fullCommandLine := fmt.Sprintf("%s %s", process.Executable(), cmdline)
+		lastModified := "NA"
+		fileExists := true
+		exePath, err2 := GetExePathFromPID(process.Pid())
+		var fileStat os.FileInfo
+		if err2 == nil {
+			if fileStat, err = os.Stat(exePath); err == nil {
+				lastModified = fileStat.ModTime().UTC().String()
+			} else if errors.Is(err, os.ErrNotExist) {
+				fileExists = false
+			} else {
+			}
+		}
+
 		tmp_ := map[string]interface{}{
-			"Commandline": fullCommandLine,
-			"PID":         strconv.Itoa(process.Pid()),
+			"Commandline":     fullCommandLine,
+			"PID":             strconv.Itoa(process.Pid()),
+			"ExeLastModified": lastModified,
+			"ExePath":         exePath,
 		}
 		detection := Detection{
 			Name:      "Suspicious Commandline",
@@ -39,30 +57,50 @@ func FindSuspiciousCommandlines(logger zerolog.Logger, detections chan<- Detecti
 			Technique: "T1059",
 			Metadata:  tmp_,
 		}
-	patternMatch:
-		for _, pattern := range suspiciousPatterns {
-			//logger.Info(pattern)
-			if helpers.SearchStringContains(fullCommandLine, pattern) {
-				detection.Name = "Suspicious Pattern in Commandline"
-				detection.Metadata["Pattern"] = pattern
+		if fileExists == false && !strings.HasSuffix(process.Executable(), "kworker/") {
+			detection.Name = "Running Process with Non-Existent File"
+			detection.Severity = 3
+			detections <- detection
+		}
+		if fileExists == true && lastModified != "NA" {
+			dayDiff := int(timestampNow.Sub(fileStat.ModTime().UTC()).Hours() / 24)
+			if dayDiff <= 30 {
+				detection.Name = "Running Binary Modified within Last 30 Days"
+				detection.Metadata["DaysAgo"] = dayDiff
+				detection.Severity = 2
 				detections <- detection
-				break patternMatch
 			}
 		}
-		ipv4Match, _ := regexp.MatchString(ipv4_regex+`|`+ipv6_regex, fullCommandLine)
-		if ipv4Match {
-			detection.Name = "IP Address Pattern in Commandline"
-			detections <- detection
+
+		result := false
+		detection.Name = "Suspicious Pattern in Commandline"
+		result = checkSuspiciousContent(detection, detections, fullCommandLine)
+		if result {
 			continue
 		}
-		domainMatch, _ := regexp.MatchString(domain_regex, fullCommandLine)
-		if domainMatch {
-			detection.Name = "Domain Pattern in Commandline"
-			detections <- detection
+		detection.Name = "IP Address Pattern in Commandline"
+		result = checkIPContent(detection, detections, fullCommandLine)
+		if result {
+			continue
+		}
+		detection.Name = "Domain Pattern in Commandline"
+		result = checkDomainContent(detection, detections, fullCommandLine)
+		if result {
 			continue
 		}
 
 		//logger.Printf("PID: %d, CMDLINE: %s", process.Pid(), fullCommandLine)
 	}
 	return
+}
+
+func GetExePathFromPID(pid int) (string, error) {
+	command := fmt.Sprintf("ls -al /proc/%v/exe", pid)
+	cmd := exec.Command(command)
+	stdout, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	exePath := strings.SplitN(string(stdout), "exe ->", 2)[1]
+	return exePath, nil
 }
